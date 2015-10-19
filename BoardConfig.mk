1e766c6bc2f91dbdda4e16dab161b6a84360bdb0
Steve Kondik <steve@cyngn.com>
2015-10-19 14:43:39 -0700

    whyred: power-libperfmgr: Add support for tap-to-wake feature control
    Squashed into this commit:
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Sat Dec 7 23:51:29 2019 +0100
    
        power: Follow symlinks when writing to dt2w toggle path
    
        * Some devices tend to symlink touchpanel related toggles
          to common path due to having to deal with different panels.
    
        Change-Id: I429e4c5acfbcd3ff26a019f919fbe880692160bd
    
    [ArianK16a]: Adapt to AIDL HAL
    
    Change-Id: I545902b29f4828c127bc32def6e30b67ce4a3aa7
    Co-authored-by: PIPIPIG233666 <pig.priv@gmail.com>
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
    Signed-off-by: v4lkyr <valkyr23@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,9 +77,6 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
-# DT2W
-TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
-
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
