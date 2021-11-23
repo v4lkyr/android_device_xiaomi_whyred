14267f007e8fbf67e82060023999c63c4937651d
Reinazhard <reinazhard@gmail.com>
2021-11-23 10:46:15 +0000

    whyred: cleanup
    Signed-off-by: Reinazhard <reinazhard@gmail.com>
    Signed-off-by: v4lkyr <valkyr23@gmail.com>

--- device.mk
+++ device.mk
@@ -118,8 +118,8 @@ PRODUCT_PACKAGES += \
     com.dsi.ant.antradio_library \
     com.dsi.ant@1.0.vendor
 
-#PRODUCT_COPY_FILES += \
-#    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
+PRODUCT_COPY_FILES += \
+    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
 # Biometrics
 PRODUCT_PACKAGES += \
@@ -407,8 +407,8 @@ PRODUCT_PACKAGES += \
     qti-telephony-utils \
     telephony-ext
 
-#PRODUCT_BOOT_JARS += \
-#    telephony-ext
+PRODUCT_BOOT_JARS += \
+    telephony-ext
 
 # Thermal
 PRODUCT_PACKAGES += \
