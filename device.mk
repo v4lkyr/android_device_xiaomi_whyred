a2e87d23c7e1c96a4e571b04378b2d1207d4ded5
clarencelol <clarencekuiek@icloud.com>
2022-05-30 23:37:17 +0800

    whyred: Partially update radio blobs from S62Pro
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: v4lkyr <valkyr23@gmail.com>

--- device.mk
+++ device.mk
@@ -379,9 +379,10 @@ PRODUCT_COPY_FILES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.5.vendor \
-    android.hardware.radio.config@1.2.vendor \
+    android.hardware.radio@1.6.vendor \
+    android.hardware.radio.config@1.3.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
+    android.hardware.secure_element@1.1 \
     android.hardware.secure_element@1.2.vendor \
     libavservices_minijail.vendor \
     libjson \
