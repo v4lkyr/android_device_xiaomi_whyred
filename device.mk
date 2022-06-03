bf3243824212cd0d99ed448af1eadd802dd072cf
v4lkyr <valkyr23@gmail.com>
2022-06-03 21:59:20 +0800

    whyred: build camera HAL from CLO
    Signed-off-by: v4lkyr <valkyr23@gmail.com>

--- device.mk
+++ device.mk
@@ -131,15 +131,23 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap
+    Snap \
+    camera.sdm660
 
 PRODUCT_PACKAGES += \
-    android.hardware.camera.device@3.5:64 \
-    android.hardware.camera.provider@2.4-impl:32 \
+    android.hardware.camera.common@1.0 \
+    android.hardware.camera.device@3.4 \
+    android.hardware.camera.provider@2.5 \
+    android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5:64 \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    vendor.qti.hardware.camera.postproc@1.0.vendor \
     libdng_sdk.vendor \
-    vendor.qti.hardware.camera.device@1.0:64
+    libcamera2ndk_vendor \
+    libgui_vendor \
+    libstdc++.vendor \
+    libxml2
 
 # Codec2 modules
 PRODUCT_PACKAGES += \
